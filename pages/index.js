7c29170044aaf6d22e5325a53318827eb4fef06f
alrightalan <alanonmail@gmail.com>
2021-03-14 01:20:37 +0100

    fixing theme toggle

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,10 @@ export default function Home() {
 				<title>Where in the world?</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<div className="min-h-screen text-dark-elements bg-light-bg dark:bg-dark-bg dark:text-light-elements">
+			<main className="min-h-screen text-dark-elements bg-light-bg dark:bg-dark-bg dark:text-light-elements">
 				<Navbar />
 				<Main />
-			</div>
+			</main>
 		</ThemeProvider>
 	);
 }
