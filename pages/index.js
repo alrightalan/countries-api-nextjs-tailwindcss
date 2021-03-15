ed8d1923c7d9f9142ab9ecaa8857169afcf2b64c
alrightalan <alanonmail@gmail.com>
2021-03-15 14:04:52 +0100

    fixing the theme handling

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import { ThemeProvider } from "../context/themeContext";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Main from "./countries/countries";
 
 export default function Home() {
 	return (
-		<ThemeProvider>
+		<>
 			<Head>
 				<title>Where in the world?</title>
 				<link rel="icon" href="/favicon.ico" />
@@ -14,6 +13,6 @@ export default function Home() {
 				<Navbar />
 				<Main />
 			</main>
-		</ThemeProvider>
+		</>
 	);
 }
