a18fb0be7a23deb98cb681c910fe163a2c02ea65
alrightalan <alanonmail@gmail.com>
2021-03-17 02:53:24 +0100

    last donuts of the night

--- context/themeToggle.js
+++ context/themeToggle.js
@@ -1,23 +1,14 @@
-import React, { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { IoSunny, IoMoon } from "react-icons/io5";
 
 
 const Toggle = () => {
 	const { theme, setTheme } = useTheme();
-	const [isMounted, setIsMounted] = useState(false);
-
-	useEffect(() => {
-		setIsMounted(true);
-	}, []);
 
 	const switchTheme = () => {
-		if (isMounted) {
-			setTheme(theme === "light" ? "dark" : "light");
-		}
-	};
+		setTheme(theme === "light" ? "dark" : "light");
 
-	console.log(isMounted);
+	};
 
 	return (
 		<div className="p-2 transition duration-500 ease-in-out rounded-full">
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,26 @@
 import "../styles/globals.css";
 import "../styles/tailwind.css";
+import { useState, useEffect } from "react";
 import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
+	console.log(isMounted);
+
 	return (
-		<ThemeProvider
-			forcedTheme={Component.theme || undefined}
-			attribute="class"
-		>
-			<Component {...pageProps} />
-		</ThemeProvider>
+		isMounted && (
+			<ThemeProvider
+				forcedTheme={Component.theme || undefined}
+				attribute="class"
+			>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		)
 	);
 }
 
