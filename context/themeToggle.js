c27bab3e340a6f08d4373ef07e60d5f9eda7e7b3
alrightalan <alanonmail@gmail.com>
2021-03-17 02:21:01 +0100

    added next/themes

--- context/themeToggle.js
+++ context/themeToggle.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { IoSunny, IoMoon } from "react-icons/io5";
 import { ThemeContext } from "./themeContext";
 
 const Toggle = () => {
 	const { theme, setTheme } = React.useContext(ThemeContext);
 
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+
+	const switchTheme = () => {
+		if (isMounted) {
+			setTheme(theme === "light" ? "dark" : "light");
+		}
+	};
+
+	console.log(isMounted);
+
 	return (
 		<div className="p-2 transition duration-500 ease-in-out rounded-full">
 			{theme === "dark" ? (
 				<IoSunny
-					onClick={() =>
-						setTheme(theme === "dark" ? "light" : "dark")
-					}
+					onClick={switchTheme}
 					className="text-2xl cursor-pointer"
 				/>
 			) : (
 				<IoMoon
-					onClick={() =>
-						setTheme(theme === "dark" ? "light" : "dark")
-					}
+					onClick={switchTheme}
 					className="text-2xl cursor-pointer"
 				/>
 			)}
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,12 @@
-import { useState, useEffect } from "react";
 import "../styles/globals.css";
 import "../styles/tailwind.css";
 import { ThemeProvider } from "../context/themeContext";
 
 function MyApp({ Component, pageProps }) {
-	const [mounted, setMounted] = useState(false);
-
-	useEffect(() => {
-		setMounted(true);
-	}, []);
-
 	return (
-		<ThemeProvider>{mounted && <Component {...pageProps} />}</ThemeProvider>
+		<ThemeProvider>
+			<Component {...pageProps} />
+		</ThemeProvider>
 	);
 }
 
