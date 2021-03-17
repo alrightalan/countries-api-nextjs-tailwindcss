621f88c27069af1f66ed7c026a5ee47b8b28fb93
alrightalan <alanonmail@gmail.com>
2021-03-17 02:56:44 +0100

    this is the one!

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import Head from "next/head";
 import Main from "./countries/countries";
+import Navbar from "../components/Navbar"
 
 export default function Home() {
 	return (
 		<>
-			<main className="text-dark-elements bg-light-bg dark:bg-dark-bg dark:text-light-elements">
-				<Head>
-					<title>Where in the world?</title>
-					<link rel="icon" href="/favicon.ico" />
-				</Head>
-				<Main />
-			</main>
+			<Head>
+				<title>Where in the world?</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Navbar />
+			<h1>Hello!</h1>
 		</>
 	);
 }
